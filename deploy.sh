810a7e460e820952d1331f854259b722ad90a809
Brendan McKenzie <brendan.mckenzie@deepend.com.au>
2016-06-02 06:22:40 +0000

    Fix CI build

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ echo "+ building package"
 zip -9 -q -r --exclude=*faker* --exclude=*.DS_Store* --exclude=*test.js* lambda.zip .
 
 echo "+ deploying to aws"
-aws lambda update-function-code --function-name contentful-wehbook --zip-file fileb://lambda.zip
+aws lambda update-function-code --function-name contentful-webhook --zip-file fileb://lambda.zip
