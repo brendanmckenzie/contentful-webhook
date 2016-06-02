b2a6248d3a7caee231c91ad6a87b9af5b73cd0c0
Brendan McKenzie <hello@brendanmckenzie.com>
2016-06-02 19:59:01 +1000

    Updating unpublish handler

--- actions/unpublish.js
+++ actions/unpublish.js
@@ -1,4 +1,5 @@
 var AWS = require('aws-sdk');
+var async = require('async');
 var utils = require('./utils');
 var config = require('../config');
 
@@ -6,43 +7,83 @@ var s3 = new AWS.S3({ region: config.region });
 
 module.exports = function (event, context, callback) {
     var id = event.body.sys.id;
+    var space = event.body.sys.space.sys.id;
 
-    console.log('retreiving index path');
-    s3.getObject({
-        Bucket: config.bucket,
-        Key: `.index/${id}`
-    }, function (err, res) {
-        if (err) {
-            return console.log('failed', err, err.stack);
-        }
-
-        var fullPath = res.Body.toString();
-
-        console.log(`deleting at path: ${fullPath}`);
-
-        var params = {
-            Bucket: config.bucket,
-            Key: fullPath
-        };
-        s3.deleteObject(params, function (err, callback) {
-            if (err) {
-                return console.log('failed', err, err.stack);
-            }
-
-            console.log('deleted');
-
-            console.log('deleting index path');
-            var params = {
+    console.log(`unpublishing '${id}'' in space '${space}'`);
+    async.waterfall([
+        function (next) {
+            console.log(`looking up space: ${space}`);
+            s3.getObject({
                 Bucket: config.bucket,
-                Key: `.index/${id}`
-            };
-            s3.deleteObject(params, function (err, callback) {
+                Key: `${space}.json`
+            }, function (err, res) {
                 if (err) {
-                    return console.log('failed', err, err.stack);
+                    return next(`space not found: ${err}\n${err.stack}`);
                 }
 
-                console.log('deleted');
+                try {
+                    var config = JSON.parse(res.Body.toString());
+                    console.log('... found');
+                    next(null, {
+                        id: event.body.sys.id,
+                        config: config
+                    });
+                }
+                catch (ex) {
+                    next(`invalid space config: ${ex}`);
+                }
             });
-        });
-    });
+        },
+
+        function (req, next) {
+            console.log('retreiving index path');
+            var targetS3 = new AWS.S3({
+                region: req.config.target.region,
+                accessKeyId: req.config.target.accessKeyId ,
+                secretAccessKey: req.config.target.secretAccessKey
+            });
+
+            targetS3.getObject({
+                Bucket: req.config.target.bucket,
+                Key: `.index/${id}`
+            }, function (err, res) {
+                if (err) {
+                    console.error('... failed', err, err.stack);
+                }
+                else {
+                    var fullPath = res.Body.toString();
+                    console.log(`deleting at path: ${fullPath}`);
+
+                    targetS3.deleteObject({
+                        Bucket: req.config.target.bucket,
+                        Key: fullPath
+                    }, function (err, callback) {
+                        if (err) {
+                            console.error('... failed', err, err.stack);
+                        }
+                        else {
+                            console.log('... deleted');
+
+                            console.log('deleting index path');
+                            targetS3.deleteObject({
+                                Bucket: req.config.target.bucket,
+                                Key: `.index/${id}`
+                            }, function (err, callback) {
+                                if (err) {
+                                    console.error('... failed', err, err.stack);
+                                }
+                                else {
+                                    console.log('deleted');
+
+                                    next(null, res);
+                                }
+                            });
+                        }
+                    });
+                }
+            });
+        }
+    ], function (err, res) {
+
+    })
 }
